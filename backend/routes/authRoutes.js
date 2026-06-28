f13adcf9a3c5e4d7b796bef977afbbefd681f886
Dhavalpandav09 <dhavalpandav4321@gmail.com>
2026-06-28 15:02:22 +0530

    chnages

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const { login, verifyToken } = require('../controllers/authController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/login',  login);
-router.get('/verify',  protect, verifyToken);
-
-module.exports = router;
--- backend/routes/statsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express    = require('express');
-const router     = express.Router();
-const { protect } = require('../middleware/authMiddleware');
-const { getStats } = require('../controllers/statsController');
-
-router.get('/', protect, getStats);
-
-module.exports = router;
